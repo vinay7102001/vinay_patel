d632a00c4c7065712a1454693190ee8a49bd6d43
vinay7102001 <86544715+vinay7102001@users.noreply.github.com>
2021-12-10 16:33:25 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,9 @@
+# Author : vinay
+''' this symbole is use for comment 
+***** first program **** ''' 
+import os # importing os module
+print("hello word") 
+print('''hello word,
+patel vinay,
+       how are you ''') 
+       
